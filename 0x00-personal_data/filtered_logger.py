7d4b1df61e10f0e162c634bd21f2ff88cd3cbc41
Pericles Adjovi <55035365+Pericles001@users.noreply.github.com>
2022-07-22 01:41:33 +0000

    Edit file

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,12 +4,11 @@ Personal data
 """
 
 
-from asyncio.log import logger
-from cgitb import handler
-from email import message
+
 import logging
 import os
 import re
+import mysql.connector
 from typing import List
 
 
@@ -18,39 +17,35 @@ PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
 def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
+    """ Replacing """
     for f in fields:
-        message = re.sub(
-            rf"{f}=(.*?)\{separator}",
-            f'{f}={redaction}{separator}',
-            message
-        )
+        message = re.sub(rf"{f}=(.*?)\{separator}",
+                         f'{f}={redaction}{separator}', message)
     return message
 
 
 class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class
-        """
+    """ RedactingFormatter class. """
 
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
+        """ Init """
         self.fields = fields
         super(RedactingFormatter, self).__init__(self.FORMAT)
 
     def format(self, record: logging.LogRecord) -> str:
-        """
-        Format
-        """
+        """ Format """
         return filter_datum(self.fields, self.REDACTION,
                             super().format(record), self.SEPARATOR)
 
 
 def get_logger() -> logging.Logger:
+    """ Implementing a logger.
     """
-    Implementing a logger
-    """
+
     logger = logging.getLogger("user_data")
     logger.setLevel(logging.INFO)
     logger.propagate = False
@@ -61,37 +56,34 @@ def get_logger() -> logging.Logger:
 
 
 def get_db() -> mysql.connector.connection.MySQLConnection:
+    """ Implement db conectivity
     """
-    Connect to secure database
-    """
-    pwd = os.environ.get("PERSONAL_DATA_DB_PASSWORD")
-    usrname = os.environ.get('PERSONAL_DATA_DB_USERNAME', "root")
+    psw = os.environ.get("PERSONAL_DATA_DB_PASSWORD", "")
+    username = os.environ.get('PERSONAL_DATA_DB_USERNAME', "root")
     host = os.environ.get('PERSONAL_DATA_DB_HOST', 'localhost')
     db_name = os.environ.get('PERSONAL_DATA_DB_NAME')
     conn = mysql.connector.connect(
         host=host,
         database=db_name,
-        user=usrname,
-        password=pwd
-    )
+        user=username,
+        password=psw)
     return conn
 
 
 def main() -> None:
-    """
-    Implement a main function
+    """ Implement a main function
     """
     db = get_db()
     cursor = db.cursor()
     cursor.execute("SELECT * FROM users;")
     for row in cursor:
         message = f"name={row[0]}; email={row[1]}; phone={row[2]}; " +\
-            f"ssn={row[3]}; password={row[4]}; ip={row[5]}; " +\
-            f"last_login={row[6]}; useer_agent={row[7]};"
+            f"ssn={row[3]}; password={row[4]};ip={row[5]}; " +\
+            f"last_login={row[6]}; user_agent={row[7]};"
         print(message)
     cursor.close()
     db.close()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
