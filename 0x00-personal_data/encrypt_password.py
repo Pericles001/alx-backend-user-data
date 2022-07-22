13c2fc2c0077c63b4f18bf92a8fc264a48b5d4cb
Pericles Adjovi <55035365+Pericles001@users.noreply.github.com>
2022-07-22 01:59:16 +0000

    Update file

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -14,8 +14,7 @@ def hash_password(password: str) -> bytes:
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
 
-def is_valid(hashed_password: bool, password: str) -> bool:
-    """
-    Password validation
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ is valid?
     """
     return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
