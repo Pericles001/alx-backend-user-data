98459f2d10652880b355ff1dc1ca1f6e19d0d100
Pericles Adjovi <55035365+Pericles001@users.noreply.github.com>
2022-07-22 01:57:09 +0000

    Edit file with is_valid method

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -12,3 +12,10 @@ def hash_password(password: str) -> bytes:
     Salted pass generation
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bool, password: str) -> bool:
+    """
+    Password validation
+    """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
