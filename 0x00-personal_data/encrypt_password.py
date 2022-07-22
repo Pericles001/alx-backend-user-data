5709d8a5517cfdcdef9a46d279a6e6cb50fb80c1
Pericles001 <periclesadjovi@gmail.com>
2022-07-22 02:51:17 +0100

    Edit file with hash password function

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Encrypting passwords
+"""
+
+
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """
+    Salted pass generation
+    """
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
