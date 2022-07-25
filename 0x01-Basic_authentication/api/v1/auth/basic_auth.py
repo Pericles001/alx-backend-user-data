fddf3c6709a19ffbb1ade7cbf95a1094d4147674
Pericles001 <periclesadjovi@gmail.com>
2022-07-26 00:46:53 +0100

    Edit file with basuc auth class

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -19,6 +19,19 @@ class BasicAuth(Auth):
         returns the Base64 part
         of the Authorization header for a Basic Authentication
         """
-        if not ah or type(ah) != str or not ah.startswith("Basic "):
+        if not authorization_header or type(authorization_header) != str or not authorization_header.startswith("Basic "):
             return
-        return "".join(ah.split(" ")[1:])
+        return "".join(authorization_header.split(" ")[1:])
+
+    def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:
+        """
+         returns the decoded value of a Base64 string base64_authorization_header
+        """
+        if not base64_authorization_header or type(base64_authorization_header) != str:
+            return
+        try:
+            b64_bytes = base64_authorization_header.encode('utf-8')
+            res = base64.b64decode(b64_bytes)
+            return res.decode('utf-8')
+        except Exception:
+            return
