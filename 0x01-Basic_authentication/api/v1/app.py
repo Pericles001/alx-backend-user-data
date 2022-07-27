5526c1829a2cadcf7c1b317638de1c9c710bc1c6
Pericles001 <periclesadjovi@gmail.com>
2022-07-27 02:05:16 +0100

    Set pycodestyle up

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -36,6 +36,7 @@ def unauthorized(error) -> str:
     """
     return jsonify({"error": "Forbidden"}), 403
 
+
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
