5d033139fe12859d208abd8fe4c1b6fc10c7a67d
Pericles001 <periclesadjovi@gmail.com>
2022-07-29 03:42:55 +0100

    Add method to decode on base 64

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -22,8 +22,23 @@ class BasicAuth(Auth):
          header for a Basic Authentication
         """
         if (authorization_header is None or
-                not isinstance(authorization_header, str)):
+                not isinstance(authorization_header, str) or
+                not authorization_header.startswith("Basic")):
             return None
-        if not authorization_header.startswith("Basic"):
-            return None
+
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """
+        returns the decoded value of a Base64
+        string base64_authorization_header
+        """
+        b64_auth_header = base64_authorization_header
+        if b64_auth_header and isinstance(b64_auth_header, str):
+            try:
+                encode = b64_auth_header.encode('utf-8')
+                base = base64.b64decode(encode)
+                return base.decode('utf-8')
+            except binascii.Error:
+                return None
