c151df561faba3d4b828211e5e6e328bce87d01c
Pericles001 <periclesadjovi@gmail.com>
2022-07-29 03:29:17 +0100

    Edit file with extract_base64 method

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -15,11 +15,14 @@ class BasicAuth(Auth):
     class BasicAuth
     """
 
-    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
         """
-         returns the Base64 part of the Authorization header for a Basic Authentication
+         returns the Base64 part of the Authorization
+         header for a Basic Authentication
         """
-        if authorization_header is None or not isinstance(authorization_header, str):
+        if (authorization_header is None or
+                not isinstance(authorization_header, str)):
             return None
         if not authorization_header.startswith("Basic"):
             return None
