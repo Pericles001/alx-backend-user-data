21c1885ed575bc0870e7db24e31e73129757fa80
Pericles001 <periclesadjovi@gmail.com>
2022-07-29 03:03:43 +0100

    Create class BasicAuth

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -5,6 +5,7 @@ Route module for the API
 from os import getenv
 from api.v1.views import app_views
 from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
@@ -17,6 +18,8 @@ auth = None
 
 if getenv("AUTH_TYPE") == "auth":
     auth = Auth()
+elif getenv("AUTH_TYPE") == "basic_auth":
+    auth = BasicAuth()
 
 
 @app.errorhandler(404)
---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Basic Auth module
+"""
+
+from api.v1.auth.auth import Auth
+from typing import TypeVar, List
+from models.user import User
+import base64
+import binascii
+
+
+class BasicAuth(Auth):
+    """
+    class BasicAuth
+    """
