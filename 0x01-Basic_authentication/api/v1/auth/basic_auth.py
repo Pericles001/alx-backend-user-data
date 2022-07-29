877d080ecedaa159f925549b1b66246e0ff1bfca
Pericles001 <periclesadjovi@gmail.com>
2022-07-29 04:10:28 +0100

    Add method to define user

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -54,3 +54,15 @@ class BasicAuth(Auth):
             res = decoded_64.split(":", 1)
             return (res[0], res[1])
         return (None, None)
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        overloads Auth and retrieves the User instance for a request
+        """
+        auth_header = self.authorization_header(request)
+        base64_sha = self.extract_base64_authorization_header(auth_header)
+        decoded_sha = self.decode_base64_authorization_header(base64_sha)
+        credentials = self.extract_user_credentials(decoded_sha)
+        user = self.user_object_from_credentials(
+            credentials[0], credentials[1])
+        return user
