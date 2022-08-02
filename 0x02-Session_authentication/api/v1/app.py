41c49fce7289cf064bfa65cd6d859ac7a0a12ab5
Pericles001 <periclesadjovi@gmail.com>
2022-08-02 01:19:25 +0100

    Fix pycodestyle

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -2,10 +2,13 @@
 """
 Route module for the API
 """
-from os import getenv
-from api.v1.views import app_views
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
+from api.v1.auth.session_auth import SessionAuth
+from api.v1.auth.session_exp_auth import SessionExpAuth
+from api.v1.auth.session_db_auth import SessionDBAuth
+from os import getenv
+from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
@@ -15,11 +18,16 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
-
-if getenv("AUTH_TYPE") == "auth":
-    auth = Auth()
-elif getenv("AUTH_TYPE") == "basic_auth":
+if os.getenv("AUTH_TYPE") == "basic_auth":
     auth = BasicAuth()
+elif os.getenv("AUTH_TYPE") == "session_auth":
+    auth = SessionAuth()
+elif os.getenv("AUTH_TYPE") == "session_exp_auth":
+    auth = SessionExpAuth()
+elif os.getenv("AUTH_TYPE") == "session_db_auth":
+    auth = SessionDBAuth()
+elif os.getenv("AUTH_TYPE") == "auth":
+    auth = Auth()
 
 
 @app.errorhandler(404)
@@ -38,7 +46,7 @@ def unauthorized(error) -> str:
 
 
 @app.errorhandler(403)
-def unauthorized(error) -> str:
+def forbidden(error) -> str:
     """
     Forbidden handler.
     """
@@ -46,18 +54,21 @@ def unauthorized(error) -> str:
 
 
 @app.before_request
-def before_request():
+def before():
     """
-    handler before_request
+    Before request.
     """
-    authorized_list = ['/api/v1/status/',
-                       '/api/v1/unauthorized/', '/api/v1/forbidden/']
-
-    if auth and auth.require_auth(request.path, authorized_list):
-        if not auth.authorization_header(request):
+    if auth:
+        paths = ['/api/v1/status/',
+                 '/api/v1/unauthorized/', '/api/v1/forbidden/',
+                 '/api/v1/auth_session/login/']
+        if not auth.require_auth(request.path, paths):
+            return
+        if (not auth.authorization_header(request) and
+                not auth.session_cookie(request)):
             abort(401)
         request.current_user = auth.current_user(request)
-        if not auth.current_user(request):
+        if not request.current_user:
             abort(403)
 
 
