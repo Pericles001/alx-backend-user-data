49704526dc5897c62042af580534b485380039ae
Pericles001 <periclesadjovi@gmail.com>
2022-08-03 01:08:14 +0100

    Edit session_auth.py with new function

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -10,3 +10,14 @@ class SessionAuth(Auth):
     """
     Session auth class
     """
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+        creates a Session ID for a user_id
+        """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        session_id = str(uuid4())
+        SessionAuth.user_id_by_session_id[user_id] = session_id
+        return session_id
