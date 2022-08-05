c704928ba49e84d5a18caac24535198bea2d81d0
Pericles001 <periclesadjovi@gmail.com>
2022-08-05 02:36:26 +0100

    Add new route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -4,7 +4,10 @@ Basif Flask app
 """
 
 from flask import Flask, jsonify
+from auth import Auth
 
+
+AUTH = Auth()
 app = Flask(__name__)
 
 
@@ -15,6 +18,21 @@ def welcome() -> str:
       - welcome
     """
     return jsonify({"message": "Bienvenue"}), 200
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def user() -> str:
+    """ POST /users
+    Return:
+      - message
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": f"{email}", "message": "user created"}), 200
+    except Exception:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
